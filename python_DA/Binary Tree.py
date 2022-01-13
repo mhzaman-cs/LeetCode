1f298101436ce2d1c67c1bc2032c9f1502db395e
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-01-12 23:52:41 -0500

    1/12/2022

--- python_DA/Binary Tree.py
+++ python_DA/Binary Tree.py
@@ -2,13 +2,36 @@
 
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
         return max(1 + self.maxDepth(root.right), 1 + self.maxDepth(root.left))
+
+
+# Question 111 (Easy) Link: https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        else:
+            if root.left is None:
+                return self.minDepth(root.right) + 1
+            elif root.right is None:
+                return self.minDepth(root.left) + 1
+            else:
+                return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
