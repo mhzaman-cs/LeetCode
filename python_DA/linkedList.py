bcc50186ad5a44f5de8dabfc53467a5d3d65c136
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-01-14 23:55:32 -0500

    1/14/2022

---
+++ python_DA/linkedList.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+#Better Memory use
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        cur = head
+
+        while cur:
+            n = cur.next
+            cur.next = prev
+            prev = cur
+            cur = n
+
+        return prev
