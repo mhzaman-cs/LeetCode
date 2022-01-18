4a8754245a7e9ca6cbc64cd9b18b05ef7138b854
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-01-17 23:55:24 -0500

    1/17/2022

--- python_DA/arrays.py
+++ python_DA/arrays.py
@@ -292,3 +292,28 @@ class Solution:
             if not dfs(i):
                 return False
         return True
+
+
+# Question 72 (Medium) Link: https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix: return 0
+        m, n = len(matrix), len(matrix[0])
+        if not n: return 0
+
+        r, r1, r2 = -1, 0, m-1
+        while r1 <= r2:
+            mid = (r1+r2) // 2
+            if matrix[mid][0] <= target <= matrix[mid][n-1]: r = mid; break
+            elif matrix[mid][0] > target: r2 = mid - 1
+            else: r1 = mid + 1
+        if r == -1: return False
+
+        c1, c2 = 0, n-1
+        while c1 <= c2:
+            mid = (c1+c2) // 2
+            if matrix[r][mid] == target: return True
+            elif matrix[r][mid] > target: c2 = mid - 1
+            else: c1 = mid + 1
+        return False
