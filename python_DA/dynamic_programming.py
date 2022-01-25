b36f308a456b04f4cfb286a7e385a017e3fd94c3
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-01-24 23:56:30 -0500

    1/24/2022

--- python_DA/dynamic_programming.py
+++ python_DA/dynamic_programming.py
@@ -16,7 +16,7 @@ class Solution:
                     dp[i + 1][j + 1] = dp[i][j] and (p[i] == s[j] or p[i] == '.')
         return dp[-1][-1]
 
-# Question 53 (Easy) Link: https://leetcode.com/problems/maximum-subarray/
+# Question 70 (Easy) Link: https://leetcode.com/problems/climbing-stairs/
 
 class Solution(object):
     def maxSubArray(self, nums):
@@ -26,3 +26,15 @@ class Solution(object):
             maxSum = max(maxSum, curSum)
 
         return maxSum
+
+# Question 53 (Easy) Link: https://leetcode.com/problems/maximum-subarray/
+
+class Solution(object):
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        res = [0 for i in xrange(n)]
+        res[0], res[1] = 1, 2
+        for i in xrange(2, n):
+            res[i] = res[i-1] + res[i-2]
+        return res[-1]
