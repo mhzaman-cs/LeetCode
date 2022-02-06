19116d7e83498efd443d290f7800618f5cd93555
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-02-05 21:59:45 -0500

    2/5/2022

--- python_DA/linkedList.py
+++ python_DA/linkedList.py
@@ -33,3 +33,28 @@ class Solution:
             curr = next
 
         return prev
+
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        dummy = jump = ListNode(0)
+        dummy.next = l = r = head
+
+        while True:
+            count = 0
+            while r and count < k:   # use r to locate the range
+                r = r.next
+                count += 1
+            if count == k:  # if size k satisfied, reverse the inner linked list
+                pre, cur = r, l
+                for _ in range(k):
+                    cur.next, cur, pre = pre, cur.next, cur  # standard reversing
+                jump.next, jump, l = pre, l, r  # connect two k-groups
+            else:
+                return dummy.next
