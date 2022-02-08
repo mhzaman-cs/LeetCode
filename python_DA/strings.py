8ee596dc1e3807a8bb7688da86efff30eefa8473
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-02-07 23:07:48 -0500

    2/7/2022

--- python_DA/strings.py
+++ python_DA/strings.py
@@ -2,3 +2,32 @@ class Solution(object):
     def isPalindrome(self, s):
         s = ''.join(e for e in s if e.isalnum()).lower()
         return s==s[::-1]
+
+
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # use 1D DP
+        # dp[i] records the longestValidParenthese EXACTLY ENDING at s[i]
+        dp = [0 for x in xrange(len(s))]
+        max_to_now = 0
+        for i in xrange(1,len(s)):
+            if s[i] == ')':
+                # case 1: ()()
+                if s[i-1] == '(':
+                    # add nearest parentheses pairs + 2
+                    dp[i] = dp[i-2] + 2
+                # case 2: (())
+                # i-dp[i-1]-1 is the index of last "(" not paired until this ")"
+                elif i-dp[i-1]-1 >= 0 and s[i-dp[i-1]-1] == '(':
+                    if dp[i-1] > 0: # content within current matching pair is valid
+                    # add nearest parentheses pairs + 2 + parentheses before last "("
+                        dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]
+                    else:
+                    # otherwise is 0
+                        dp[i] = 0
+                max_to_now = max(max_to_now, dp[i])
+        return max_to_now
