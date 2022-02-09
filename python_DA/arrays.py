afc5144e879353df60ab0fbbd51288305becbef1
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-02-08 22:27:11 -0500

    2/8/2022

--- python_DA/arrays.py
+++ python_DA/arrays.py
@@ -387,3 +387,27 @@ class Solution(object):
                 nums1[m + n - 1] = nums2[n - 1]
                 n -= 1
         nums1[:n] = nums2[:n]
+
+
+
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        for i in xrange(len(nums)):
+            while 0 <= nums[i]-1 < len(nums) and nums[nums[i]-1] != nums[i]:
+                tmp = nums[i]-1
+                nums[i], nums[tmp] = nums[tmp], nums[i]
+        for i in xrange(len(nums)):
+            if nums[i] != i+1:
+                return i+1
+        return len(nums)+1
+
+    # O(nlgn) time
+    def firstMissingPositive(self, nums):
+        nums.sort()
+        res = 1
+        for num in nums:
+            if num == res:
+                res += 1
+        return res
+        
