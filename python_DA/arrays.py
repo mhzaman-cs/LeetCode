96e36f4b4b6074561b8dc69a0c315690c172b919
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-02-21 23:27:41 -0500

    2/21/2022

--- python_DA/arrays.py
+++ python_DA/arrays.py
@@ -410,4 +410,14 @@ class Solution(object):
             if num == res:
                 res += 1
         return res
-        
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+        nums1[:n] = nums2[:n]
