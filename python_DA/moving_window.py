ee90e3977496b9ef48b5e53812275fda3a3c30eb
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-02-26 22:11:37 -0500

    2/26/2022

--- python_DA/moving_window.py
+++ python_DA/moving_window.py
@@ -38,4 +38,32 @@ class Solution:
                     subd = {}
                     count = 0
 
-        return ans 
+        return ans
+
+
+
+
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution(object):
+    def cloneGraph(self, node):
+        if not node:
+            return node
+        m, visited, stack = dict(), set(), deque([node])
+        while stack:
+            n = stack.pop()
+            if n in visited:
+                continue
+            visited.add(n)
+            if n not in m:
+                m[n] = Node(n.val)
+            for neigh in n.neighbors:
+                if neigh not in m:
+                    m[neigh] = Node(neigh.val)
+                m[n].neighbors.append(m[neigh])
+                stack.append(neigh)
+        return m[node]
