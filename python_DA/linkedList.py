86c1899d342818dd9b5a95cc0d1689900e792b19
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-03-10 23:25:41 -0500

    3/10/2022

--- python_DA/linkedList.py
+++ python_DA/linkedList.py
@@ -88,3 +88,17 @@ class Solution(object):
                 return [i, seen[remaining]]
             else:
                 seen[value] = i
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr != None:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        return prev
