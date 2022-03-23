85c068cbcfea3d6b82ab6cc99e532595397f2fbd
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-03-22 23:13:57 -0400

    3/22/2022

--- python_DA/Binary Tree.py
+++ python_DA/Binary Tree.py
@@ -77,4 +77,18 @@ class Solution(object):
             return l
         r.next = self.merge(l, r.next)
         return r
-        
+
+# Question 104 (Easy) Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return max(1 + self.maxDepth(root.right), 1 + self.maxDepth(root.left))
