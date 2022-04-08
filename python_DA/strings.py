ce88d20fd96d2c24c280e4b69b946301b7e47d3b
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-04-07 23:57:16 -0400

    4/7/2022

--- python_DA/strings.py
+++ python_DA/strings.py
@@ -235,3 +235,21 @@ class Solution(object):
             curr_moves += overlay_arr[i]
             res = min(res, curr_moves)
         return res
+
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        L = [''] * numRows
+        index, step = 0, 1
+
+        for x in s:
+            L[index] += x
+            if index == 0:
+                step = 1
+            elif index == numRows -1:
+                step = -1
+            index += step
+
+        return ''.join(L)
