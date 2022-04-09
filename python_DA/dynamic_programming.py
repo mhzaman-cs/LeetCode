2fbeeaa74ee7060a7abd8302d503c70e6c9fd960
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-04-08 23:53:33 -0400

    4/8/2022

--- python_DA/dynamic_programming.py
+++ python_DA/dynamic_programming.py
@@ -163,3 +163,12 @@ class Solution(object):
             maxSum = max(maxSum, curSum)
 
         return maxSum
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        curSum = maxSum = nums[0]
+        for num in nums[1:]:
+            curSum = max(num, curSum + num)
+            maxSum = max(maxSum, curSum)
+
+        return maxSum
