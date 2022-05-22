e6350734cfc3396ef466dd85c7bc7313284120fa
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-05-22 16:44:28 -0400

    5/22/2022

--- python_DA/Work Term 1/easy.py
+++ python_DA/Work Term 1/easy.py
@@ -24,7 +24,6 @@ class Solution(object):
 
 # Iterative Solution Depth First Search
 # Definition for a binary tree node.
-
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -67,8 +66,6 @@ class TreeNode(object):
         self.left = left
         self.right = right
 
-
-
 class Solution(object):
 
     def isSameTree(self, p, q):
@@ -93,3 +90,26 @@ class Solution(object):
                 Deque.append((p_tree.left, q_tree.left))
 
         return True
+
+# Question 1 Link: https://leetcode.com/problems/two-sum/
+
+# Original Solution
+class Solution(object):
+    def twoSum(self, nums, target):
+
+        diff_hash = {} # Difference of the numbers to the target
+        len_list = len(nums)
+
+        for i in range(len_list):
+            diff_target = target - nums[i]
+            if (diff_target in diff_hash) and (diff_target == nums[i]) :
+                return [diff_hash[diff_target], i] # If 2 things need the same number to reach the target and they are equal to the, they can use eachother
+            else:
+                diff_hash[diff_target] = i
+
+        # Now we have an insurance of uniqueness as the previous case would have returned any non unique value
+
+        print(diff_hash)
+        for num_needed in diff_hash:
+            if (num_needed in nums) and (diff_hash[num_needed] != nums.index(num_needed)):
+                return [diff_hash[num_needed], nums.index(num_needed)]
