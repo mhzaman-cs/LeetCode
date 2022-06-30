2249d4b5a4f873117273dfce40ca08714fb18a41
Muhammad Hamza Zaman <mh.zaman.4069@gmail.com>
2022-06-30 00:00:51 -0400

    6/30/2022

--- python_DA/Grokking the Coding Interview/sliding_window.py
+++ python_DA/Grokking the Coding Interview/sliding_window.py
@@ -57,3 +57,116 @@ class Solution:
                     else:
                         break
         return minLen
+
+#<--------------------------- Longest Substring with maximum K Distinct Characters (medium) --------------------------->#
+
+# Grokking Passed
+def longest_substring_with_k_distinct(str1, k):
+  startIndex = strLen = maxLen = 0
+  visitedChars = {} # Hashmap for the number of chars present
+
+  if k == 0:
+    return 0
+
+  for endIndex in range(len(str1)):
+    curChar = str1[endIndex]
+
+    if curChar in visitedChars:
+      visitedChars[curChar] += 1
+      strLen += 1
+      maxLen = max(maxLen, strLen)
+
+    elif (k == 0):
+      startChar = str1[startIndex]
+      startCharsLeft = visitedChars.pop(startChar, 0)
+      print([startIndex, endIndex, str1, startChar, startCharsLeft])
+      maxLen = max(maxLen, strLen)
+
+      while(startCharsLeft != 0):
+        curStartChar = str1[startIndex]
+        if curStartChar == startChar:
+          startCharsLeft -= 1
+        else:
+          visitedChars[curStartChar] -= 1
+        startIndex += 1
+        strLen -= 1
+
+      visitedChars[curChar] = 1
+      strLen += 1
+      maxLen = max(maxLen, strLen)
+
+    else:
+      visitedChars[curChar] = 1
+      k -= 1
+      strLen += 1
+      maxLen = max(maxLen, strLen)
+
+
+  return maxLen
+
+
+#<--------------------------- Fruits into Baskets (medium) --------------------------->#
+
+# Grokking Passed
+def fruits_into_baskets(fruits):
+  startIndex = maxLen = 0
+  fruitsPicked = {}
+
+  if len(fruits) == 0:
+    return 0
+
+  for endIndex in range(len(fruits)):
+    curFruit = fruits[endIndex]
+
+    if curFruit not in fruitsPicked:
+      fruitsPicked[curFruit] = 0
+
+    fruitsPicked[curFruit] += 1
+
+    while(len(fruitsPicked) > 2):
+      curStartFruit = fruits[startIndex]
+
+      fruitsPicked[curStartFruit] -= 1
+
+      if fruitsPicked[curStartFruit] == 0:
+        del fruitsPicked[curStartFruit]
+
+      startIndex += 1
+
+    maxLen = max(maxLen, (endIndex - startIndex + 1))
+
+  return maxLen
+
+
+# Leetcode Question 904: https://leetcode.com/problems/fruit-into-baskets/
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        startIndex = maxLen = 0
+        fruitsPicked = {}
+
+        if len(fruits) == 0:
+            return 0
+
+        for endIndex in range(len(fruits)):
+            curFruit = fruits[endIndex]
+
+            if curFruit not in fruitsPicked:
+                fruitsPicked[curFruit] = 0
+
+            fruitsPicked[curFruit] += 1
+
+
+            while(len(fruitsPicked) > 2):
+                curStartFruit = fruits[startIndex]
+
+                fruitsPicked[curStartFruit] -= 1
+
+                if fruitsPicked[curStartFruit] == 0:
+                    del fruitsPicked[curStartFruit]
+
+                startIndex += 1
+
+
+            maxLen = max(maxLen, (endIndex - startIndex + 1))
+
+        return maxLen
